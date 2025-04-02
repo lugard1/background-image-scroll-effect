d7d6064a28d26bcd7eff3742f3b1015a1a88b97d
Lugard Agu <107192340+lugard1@users.noreply.github.com>
2025-04-02 21:12:51 +0100

    Update index.js since pageY is deprecated we replace with scrollY

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const bgImageEl = document.getElementById('bg-image')
+const bgImageEl = document.getElementById('bg-image');
 
-window.addEventListener('scroll', ()=>{
-  updateImage()
-})
+window.addEventListener('scroll', () => {
+  updateImage();
+});
 
-function updateImage(){
-  bgImageEl.style.opacity = 1 - window.pageYOffset/900;
-  bgImageEl.style.backgroundSize = 160 - window.pageYOffset/12 + "%"
+function updateImage() {
+  const scrollOffset = window.scrollY; // Replaces `pageYOffset`
+  
+  bgImageEl.style.opacity = 1 - scrollOffset / 900;
+  bgImageEl.style.backgroundSize = (160 - scrollOffset / 12) + "%";
 }
